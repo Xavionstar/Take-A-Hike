80c83503a94dca62cffef51fc1299dbeb3958efa
bulk <harryhamlin@gmail.com>
2023-02-23 15:56:32 -0800

    made a slight adjustment to the models to include foreign keys and adjusted relationships slightly

--- models/User.js
+++ models/User.js
@@ -3,40 +3,39 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
-    // checkPassword(loginPw) {
-    //     return true; //bcrypt.compareSync(loginPw, this.password);
-    //   }
+  // checkPassword(loginPw) {
+  //     return true; //bcrypt.compareSync(loginPw, this.password);
+  //   }
 }
 
 User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-          },
-          email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-          password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-          user_id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: true,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'user',
-      },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  },
 );
 
 module.exports = User;
