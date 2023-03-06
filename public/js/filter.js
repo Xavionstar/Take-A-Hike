ec2b6544280168198171285a7ca6ece2328aad00
bulk <harryhamlin@gmail.com>
2023-03-06 11:53:15 -0800

    added code fixing the undefined issue in the distance filter

--- public/js/filter.js
+++ public/js/filter.js
@@ -14,8 +14,9 @@ const filterHandler = async (event) => {
     //<------ grabs values associated with each drop down menu and assigns the query parameter as a string to a variable 'query' ------>
     if (loc.value) { location = `location=${loc.value}&` } else { location = "" };
     if (diff.value) { difficulty = `difficulty=${diff.value}&` } else { difficulty = "" };
-    if (length === 0) { lengthQuery = `lengthLl=0&lengthUl=3` } else if (length === 3) { lengthQuery = `lengthLl=3&lengthUl=10` } else if (length === 10) { lengthQuery=`lengthLl=10&lengthUl=100` } else { length = "" };
+    if (length == 0) { lengthQuery = `lengthLl=0&lengthUl=3` } else if (length == 3) { lengthQuery = `lengthLl=3&lengthUl=10` } else if (length == 10) { lengthQuery=`lengthLl=10&lengthUl=100` } else { lengthQuery = "" };
     const query = `/viewhikes?${location}${difficulty}${lengthQuery}`;
+    console.log(query)
     // <------ replaces page with url 'query' thus running the 'viewhikes' route on homeroutes with (or without) query parameters ------>
     document.location.replace(query);
 };
