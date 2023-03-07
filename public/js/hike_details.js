a2675fdb4ae443bd5548117f75e948e3d55d9992
Brianna Ligon <desmondsaelee@hotmail.com>
2023-03-06 18:26:19 -0800

    fixed bugs for adding and deleting comments

--- public/js/hike_details.js
+++ public/js/hike_details.js
@@ -1,6 +1,6 @@
 // set up public folder. 
 // set up sessions and target the textbox in the comments. id specific. api requests, post request.
-console.log("test")
+
 
 // Desmonds code. attaching comment to page with on click submit button.
 async function newFormHandler(event) {
@@ -10,8 +10,8 @@ async function newFormHandler(event) {
     let hike_id = targetEl.dataset.hikeid;
     
     // const hike_id = document.querySelector('#commentData').value;
-    // Send fetch request to add a new dish
-    const response = await fetch(`api/comment/${hike_id}`, {
+    // Send fetch request to add a new comment
+    const response = await fetch(`/api/comment/${hike_id}`, {
       method: 'POST',
       body: JSON.stringify({
       content,
@@ -21,48 +21,49 @@ async function newFormHandler(event) {
         'Content-Type': 'application/json',
       },
     });
-    //if the dish is added, the 'all' template will be rerendered
+    //if the comment is added, the 'all' template will be rerendered
     if (response.ok) {
       document.location.replace(`/hike/${hike_id}`);
     } else {
+      
       alert('Failed to add comment');
     }
   }
   
   document.querySelector('#commentForm').addEventListener('submit', newFormHandler);
 
-
-async function newRatingHandler(event) {
-    event.preventDefault();
-    let targetEl=document.querySelector('#ratingButton');
-    let selectOption= document.querySelector('#selectBox');
-    const rating = selectOption.value;
-    let hike_id = targetEl.dataset.hikeid;
+//  rating code we are no longer using.
+// async function newRatingHandler(event) {
+//     event.preventDefault();
+//     let targetEl=document.querySelector('#ratingButton');
+//     let selectOption= document.querySelector('#selectBox');
+//     const rating = selectOption.value;
+//     let hike_id = targetEl.dataset.hikeid;
     
     // console.log(rating);S
     // console.log(hike_id);
     
     // const hike_id = document.querySelector('#commentData').value;
     // Send fetch request to add a new rating
-    const response = await fetch(`/api/hike/${hike_id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-      rating,
+  //   const response = await fetch(`/api/hike/${hike_id}`, {
+  //     method: 'PUT',
+  //     body: JSON.stringify({
+  //     rating,
       
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+  //     }),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
    
-    if (response.ok) {
-      document.location.replace(`/api/hike/${hike_id}`);
-    } else {
-      alert('Failed to add comment');
-    }
-  }
+  //   if (response.ok) {
+  //     document.location.replace(`/api/hike/${hike_id}`);
+  //   } else {
+  //     alert('Failed to add comment');
+  //   }
+  // }
   
-  document.querySelector('#ratingButton').addEventListener('click', newRatingHandler);
+  // document.querySelector('#ratingButton').addEventListener('click', newRatingHandler);
 
 
   async function deleteComment(event) {
@@ -70,11 +71,10 @@ async function newRatingHandler(event) {
     let comment_id = document.querySelector('#deleteButton').dataset.commentid; 
     
     // const hike_id = document.querySelector('#deleteButton').dataset.hikeid;
-    const hike_id =1
+    const hike_id = document.location.href.split("/")
 
-    console.log(comment_id);
-    console.log(hike_id);
-    // Send fetch request to add a new dish
+
+    // Send fetch request to add a new comment
     const response = await fetch(`/api/comment/${comment_id}`, {
       method: 'DELETE',
       headers: {
@@ -82,14 +82,17 @@ async function newRatingHandler(event) {
       },
     });
     console.log(response);
-    //if the dish is added, the 'all' template will be rerendered
+    //if the comment is added, the 'all' template will be rerendered
     if (response.ok) {
-      document.location.replace(`/hike/${hike_id}`);
+      document.location.replace(`/hike/${hike_id[4]}`);
+      console.log(hike_id)
     } else {
       alert('Failed to delete comment');
     }
   }
   console.log(document.querySelector('#deleteButton'))
   document.querySelector('#deleteButton').addEventListener('submit', deleteComment);
+
+  // to delete chosen comment. Make it so it brings you to specific comment page/view and have delete button there and delete that comment.
     
     
